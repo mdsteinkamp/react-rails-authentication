b752c4b8f0b3db4ea586f5d4976e22935a9b5612
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-03-18 16:25:43 -0700

    please log in renders in LandingPage if user not logged in

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,8 +1,17 @@
+import { useState, useContext } from "react"
+import { Link } from "react-router-dom"
+import { UserContext } from "./UserContext"
+
+
 export default function LandingPage() {
+  const {user, setUser} = useContext(UserContext)
+
+  if (!user) return <h1>Please log in!</h1>
 
   return(
     <div>
       <h1>Landing Page</h1>
+      <h3>Welcome {user.email}</h3>
     </div>
   )
 }
